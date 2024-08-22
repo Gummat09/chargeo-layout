3778247a8fe6b458a3856af7b8776ddb5f04701c
scelo <scelogumede95@gmail.com>
2024-08-22 15:07:25 +0200

    Test

--- src/Categories.js
+++ src/Categories.js
@@ -3,6 +3,8 @@ export default function Categories() {
     <div className="container">
       <h1 className="text-base">Hello World</h1>
       <p className="underline">Hello This is a new paragraph</p>
+
+      <p>Testng</p>
     </div>
   );
 }
